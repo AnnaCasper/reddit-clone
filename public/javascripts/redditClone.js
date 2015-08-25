53f432fce4d16a2342d95c06cb31e618c72029fa
Anna Casper <anna.casper3@gmail.com>
2015-08-25 12:51:19 -0600

    most styling is complete

---
+++ public/javascripts/redditClone.js
@@ -0,0 +1,28 @@
+var app = angular.module('redditClone', []);
+
+app.controller('RedditCloneController', function ($scope) {
+  $scope.form = 0;
+  $scope.showForm = function () {
+    $scope.form = $scope.form + 1
+  };
+  $scope.posts = [
+                  {
+                    title: 'Aspen, CO',
+                    image: 'http://www.aspentimes.com/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=G4ozY9c8$ffKfbszTDHZZs$daE2N3K4ZzOUsqbU5sYv9YMsgLOEcUeu5NB$iPN4iWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg',
+                    votes: 0,
+                    description: "Sartorial Truffaut 3 wolf moon synth typewriter deep v gastropub. Before they sold out fixie kogi, pop-up mlkshk tilde polaroid cold-pressed. Try-hard plaid you probably haven't heard of them McSweeney's retro narwhal, cardigan art party Carles gluten-free pop-up whatever PBR&B. Pitchfork VHS mixtape, cold-pressed craft beer hoodie yr four loko irony asymmetrical. Fingerstache heirloom mlkshk, aesthetic stumptown fanny pack occupy lumbersexual ugh 90's drinking vinegar food truck tousled messenger bag Intelligentsia. Bespoke ennui tofu, mumblecore migas four loko VHS Vice Tumblr banh mi literally. Art party meggings semiotics, meh ethical fixie actually quinoa.",
+                    date: new Date,
+                    author: 'Anna Casper',
+                    comments: []
+                  },
+                  {
+                    title: 'Maroon Bells, CO',
+                    image: 'https://cdn.photographylife.com/wp-content/uploads/2010/11/Sunrise-at-Maroon-Bells.jpg',
+                    votes: 0,
+                    description: "Sartorial Truffaut 3 wolf moon synth typewriter deep v gastropub. Before they sold out fixie kogi, pop-up mlkshk tilde polaroid cold-pressed. Try-hard plaid you probably haven't heard of them McSweeney's retro narwhal, cardigan art party Carles gluten-free pop-up whatever PBR&B. Pitchfork VHS mixtape, cold-pressed craft beer hoodie yr four loko irony asymmetrical. Fingerstache heirloom mlkshk, aesthetic stumptown fanny pack occupy lumbersexual ugh 90's drinking vinegar food truck tousled messenger bag Intelligentsia. Bespoke ennui tofu, mumblecore migas four loko VHS Vice Tumblr banh mi literally. Art party meggings semiotics, meh ethical fixie actually quinoa.",
+                    date: new Date,
+                    author: 'Emily Brown',
+                    comments: []
+                  }
+                ];
+})
